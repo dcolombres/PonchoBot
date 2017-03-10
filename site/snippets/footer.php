bf5eac904c8ce41f4c1b5b59c0bc5b42be77e6fb
levihernan <levi.hernan@gmail.com>
2017-03-10 14:17:22 -0300

    added clipboard.js

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,7 @@
 
   <?= js('assets/js/jquery.min.js') ?>
   <?= js('assets/js/copiarCodigo.js') ?>
+  <?= js('assets/js/clipboard.js') ?>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
