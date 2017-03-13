0a8f938d2fa8592c3d6beac8a6f52f33b1210f82
saresq <santiago.aresq@gmail.com>
2017-03-13 14:13:18 -0300

    Mejora UI

--- site/templates/landing.php
+++ site/templates/landing.php
@@ -131,9 +131,10 @@
 
 <!-- <a style="position:fixed; bottom:10px; right:10px" class="btn bg-uva btn-primary"href="#" id="copiarCodigo"><i class="fa fa-copy"></i>&nbsp; Codigo</a> -->
 <div id="footer" style="position: fixed; bottom: 0; width: 100%;" class="align-center text-right">
-<button id="copy" onclick="copy(htmlCode)" class="btn bg-uva btn-primary"><i class="fa fa-copy"></i>&nbsp; Codigo</button>
+<button id="copy" onclick="copy(htmlCode)" class="btn bg-warning btn-primary"><i class="fa fa-copy"></i>&nbsp; Codigo</button>
 <a href="<?= $site->url() ?>/panel" class="btn bg-uva btn-primary"><i class="fa fa-sign-in"></i></a>
-<a href="<?= $site->url() ?>" class="btn bg-uva btn-primary"><i class="fa fa-home"></i></a>
+<a href="<?= $site->url() ?>" class="btn bg-cielo btn-primary"><i class="fa fa-home"></i></a>
+&nbsp;&nbsp;
 </div>
 <pre id="htmlCode">
 &lt;section class="jumbotron" style="background-image: url('<?= $page->background()->text() ?>');"&gt;
