eb2942f42b8556fd73b1d72c5d02abbf4ca8b16f
levihernan <levi.hernan@gmail.com>
2017-03-15 12:48:40 -0300

    Agrega scrollreveal.js

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,6 +2,15 @@
   <?= js('assets/js/jquery.min.js') ?>
   <?= js('assets/js/copiarCodigo.js') ?>
   <?= js('assets/js/clipboard.js') ?>
+  <?= js('assets/js/scrollreveal.js') ?>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script type="text/javascript">
+    window.sr = ScrollReveal();
+    sr.reveal('.jumbotron_body')
+    sr.reveal('.panel');
+    sr.reveal('.interesar');
+    sr.reveal('.main img');
+    sr.reveal('.main h2');
+  </script>
 </body>
 </html>
