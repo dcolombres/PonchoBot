92f0521ad7720562752390b0edff14e16d1db255
levihernan <levi.hernan@gmail.com>
2017-03-16 15:36:09 -0300

    chau script de copy

--- site/templates/landing.php
+++ site/templates/landing.php
@@ -176,18 +176,3 @@
 
   <pre id="htmlCode"><?php snippet('htmlCode') ?></pre>
   <?php snippet('footer') ?>
-
-  <script type="text/javascript">
-    function copy (node) {
-      let range = document.createRange();
-      range.selectNode(node);
-      window.getSelection().addRange(range);
-
-      let support = document.execCommand('copy');
-      if (support) {
-        console.info('Código copiado al portapeles');
-      } else {
-        console.error('Tu navegador no soporta execCommand');
-      }
-    }
-  </script>
