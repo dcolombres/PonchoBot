73a4ee6b159b0bb96710e9762dc4abbb827988cd
levihernan <levi.hernan@gmail.com>
2017-03-16 12:47:45 -0300

    columns

--- site/accounts/batman.php
+++ site/accounts/batman.php
@@ -7,8 +7,8 @@ email: mail@gmail.com
 language: en
 role: admin
 history:
+  - landings/testing/texto
   - landings/testing/asdftt
   - landings/testing/sectiontextimg
   - landings/testing/asdftt/asdf
   - landings/testing
-  - landings/prueba-infografia/infografia
