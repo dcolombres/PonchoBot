efaf818dbb75e6eca7b250449e7a9f8fa302414a
levihernan <levi.hernan@gmail.com>
2017-03-17 17:20:48 -0300

    poncho template de trámite con stickUp

--- site/templates/tramite.php
+++ site/templates/tramite.php
@@ -2,78 +2,62 @@
 <?php snippet('ponchoHeader') ?>
 <title>PonchoBot</title>
 
-<span id="main">
-  <main class="main" role="main">
-
-    <!-- <section class="jumbotron" style="background-image: url('<?= $page->background()->text() ?>');">
-    <div class="jumbotron_body">
+<main class="main" role="main">
     <div class="container">
-    <div class="row">
-    <div class="col-xs-12 col-md-8 col-md-offset-2 text-center">
-  </div>
-</div>
-</div>
-</div>
-<div class="overlay"></div>
-</section> -->
-
-<section>
-  <div class="container">
-    <span class="text-center">
-      <h1><?= $page->titulo()->html() ?></h1>
-    </span>
-    <?= $page->texthtml()->html() ?>
-
-    <hr>
-
-    <div class="row">
-      <div class="col-sm-3">
-            <?php foreach($page->children() as $section): ?>
-                <p><a href="#<?= $section->identificador() ?>"> <?= $section->titulo() ?> </a></p>
-            <?php endforeach ?>
-      </div>
-      <div class="col-sm-9">
-        <?php foreach($page->children() as $section): ?>
-          <div id="<?= $section->identificador()?>"class="p-b-1">
-            <h2><?= $section->titulo() ?></h2>
-            <?= $section->texthtml()->html() ?>
-          </div>
-        <?php endforeach ?>
-      </div>
-
-    </div>
-
-
-
-  </div>
-</section>
-
-<?php if ($page->footer()!=''): ?>
-  <section style="background: #fff">
-    <div class="container">
-      <div class="text-left">
-        <h2>También te puede interesar...</h2>
-      </div>
-      <?php
-      $footerTags = explode(",", $page->footer());
-      ?>
       <div class="row">
-        <?php for ($i=0; $i < count($footerTags) ; $i++): ?>
-          <div class="col-md-4 interesar">
-            <a href="<?php echo $site->page('footers/' . $footerTags[$i])->linkurl() ?>">
-              <h5><?php echo $site->page('footers/' . $footerTags[$i])->title() ?></h5>
-              <p class="text-muted"><?php echo $site->page('footers/' . $footerTags[$i])->text() ?></p>
-            </a>
-          </div>
-        <?php endfor ?>
+        <div class="col-md-push-4 col-md-8">
+          <h3 class="text-uppercase"><?= $page->titular()->html() ?></h3>
+          <p class="text-muted"><?= $page->titulo()->html() ?></p>
+          <hr>
+          <?php foreach($page->children() as $section): ?>
+            <div id="<?= $section->identificador()?>"class="p-b-1">
+              <h2><?= $section->titulo() ?></h2>
+              <?= $section->texthtml()->html() ?>
+            </div>
+          <?php endforeach ?>
+        </div>
+        <aside class="col-md-4 col-md-pull-8">
+          <section>
+            <nav class="page-sidebar">
+              <ul class="nav nav-pills nav-stacked">
+                <li class="btn-title"><a href="#">Índice</a></li>
+                <?php $i = 0 ?>
+                <?php foreach($page->children() as $section): ?>
+                  <?php $i = $i + 1 ?>
+                  <li class="index-item"><a href="#<?= $section->identificador() ?>"><strong><?= $i . '- ' ?><?= $section->titulo() ?> </strong></a></li>
+                <?php endforeach ?>
+              </ul>
+            </nav>
+          </section>
+        </aside>
       </div>
     </div>
-  </section>
-<?php endif ?>
+
+  <?php if ($page->footer()!=''): ?>
+    <section style="background: #fff">
+      <div class="container">
+        <div class="text-left">
+          <h2>También te puede interesar...</h2>
+        </div>
+        <?php
+        $footerTags = explode(",", $page->footer());
+        ?>
+        <div class="row">
+          <?php for ($i=0; $i < count($footerTags) ; $i++): ?>
+            <div class="col-md-4 interesar">
+              <a href="<?php echo $site->page('footers/' . $footerTags[$i])->linkurl() ?>">
+                <h5><?php echo $site->page('footers/' . $footerTags[$i])->title() ?></h5>
+                <p class="text-muted"><?php echo $site->page('footers/' . $footerTags[$i])->text() ?></p>
+              </a>
+            </div>
+          <?php endfor ?>
+        </div>
+      </div>
+    </section>
+  <?php endif ?>
 
 
 </main>
-</span>
 
 <!-- <a style="position:fixed; bottom:10px; right:10px" class="btn bg-uva btn-primary"href="#" id="copiarCodigo"><i class="fa fa-copy"></i>&nbsp; Codigo</a> -->
 <div id="footer" style="position: fixed; bottom: 0; width: 100%;" class="align-center text-right">
