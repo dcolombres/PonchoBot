640d29479e637a33d3312a0f28c9988816559c2d
levihernan <levi.hernan@gmail.com>
2017-03-17 13:46:24 -0300

    scrollreveal

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -5,16 +5,18 @@
   <?= js('assets/js/scrollreveal.js') ?>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <script type="text/javascript">
-    window.sr = ScrollReveal();
-    sr.reveal('.jumbotron_body')
-    sr.reveal('.panel');
+    window.sr = ScrollReveal({
+      reset: true
+    });
+    sr.reveal('.jumbotron_body', {reset: false})
+    sr.reveal('.panel', { duration: 200 }, 100);
     sr.reveal('.interesar');
     sr.reveal('.main img');
-    sr.reveal('.main p');
-    sr.reveal('.main h1');
-    sr.reveal('.main h2');
-    sr.reveal('.main h3');
-    sr.reveal('.main h4');
+    // sr.reveal('.main p');
+    // sr.reveal('.main h1');
+    // sr.reveal('.main h2');
+    // sr.reveal('.main h3');
+    // sr.reveal('.main h4');
     sr.reveal('.row')
   </script>
 </body>
