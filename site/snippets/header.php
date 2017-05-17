79a1f4241c622e263571f06aa3cbe8026fd7f988
levihernan <levi.hernan@gmail.com>
2017-05-17 15:42:16 -0300

    Robot no index

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -12,6 +12,8 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <?= css('assets/css/font-awesome.css') ?>
 
+  <meta name="robots" content="noindex">
+  <meta name="googlebot" content="noindex">
 
 </head>
 <body>
