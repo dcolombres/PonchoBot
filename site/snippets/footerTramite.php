f7553ddd8a3b435e933d1272e910e715ddea3a82
levihernan <levi.hernan@gmail.com>
2017-06-29 13:39:07 -0300

    added listjs.js

--- site/snippets/footerTramite.php
+++ site/snippets/footerTramite.php
@@ -3,6 +3,7 @@
 <?= js('assets/js/jquery.scrollto.js') ?>
 <?= js('assets/js/stickyNav.js') ?>
 <?= js('assets/js/embdmap.js') ?>
+<?= js('assets/js/listjs.js') ?>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 
