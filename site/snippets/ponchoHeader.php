42aa1358c73330e6300d9e445d38a1af1b930cbf
levihernan <levi.hernan@gmail.com>
2017-06-29 17:59:17 -0300

    header a home

--- site/snippets/ponchoHeader.php
+++ site/snippets/ponchoHeader.php
@@ -4,7 +4,7 @@
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-          <a class="navbar-brand" style="overflow: visible;" href="#"><img src="<?= $site->image('logoMinisterio.png')->url() ?>" height="50" alt=""></a>
+          <a class="navbar-brand" style="overflow: visible;" href="<?= $site->url() ?>"><img src="<?= $site->image('logoMinisterio.png')->url() ?>" height="50" alt=""></a>
           <button type="button" class="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span class="sr-only">Burger</span>
             <span class="icon-bar"></span>
