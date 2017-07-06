f89c1e43fadf1a7d25f9bba4135255030ce10ae3
levihernan <levi.hernan@gmail.com>
2017-07-06 13:30:23 -0300

    fixes varios + dev

--- site/widgets/botones/botones.php
+++ site/widgets/botones/botones.php
@@ -4,7 +4,7 @@ return array(
   'title' => 'Links útiles',
   'html' => function() {
     return '
-    <h2><a target="_blank" class="btn btn-rounded " href="http://ponchobot.produccion.gob.ar">PonchoBot</a></h2><br>
+    <h2><a target="_blank" class="btn btn-rounded " href="http://ponchobot.produccion.gob.ar">Home PonchoBot</a></h2><br>
     <h2><a target="_blank" class="btn btn-rounded " href="http://manual.produccion.gob.ar">Manual de Marca</a></h2><br>
     <h2><a target="_blank" class="btn btn-rounded " href="http://produccion.gob.ar">Web Producción</a></h2>
     ';
